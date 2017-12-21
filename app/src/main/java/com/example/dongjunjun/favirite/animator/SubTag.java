e1d90d862b94bd08335013954781f97312f9867b
liling <cxksliling@gmail.com>
2017-12-21 15:27:00 +0800

    加入二级标签

--- app/src/main/java/com/example/dongjunjun/favirite/animator/SubTag.java
+++ app/src/main/java/com/example/dongjunjun/favirite/animator/SubTag.java
@@ -1,5 +1,7 @@
 package com.example.dongjunjun.favirite.animator;
 
+import android.graphics.Paint;
+
 /**
  * 二级标签
  * Created by dongjunjun on 2017/12/13.
@@ -7,8 +9,87 @@ package com.example.dongjunjun.favirite.animator;
 
 public class SubTag extends Tag {
 
+    private BallInfo leftBall;
+    private RectInfo rightRect;
+    private RectInfo centerRect;
+    private String text;
+    private Paint mTextPaint;
+    private Paint mBgCenterPaint;
+    private Paint mBgOtherPaint;
+
     public SubTag(float x, float y) {
         super(x, y);
     }
+
+
+    public SubTag(float x, float y, BallInfo leftBall, RectInfo rightRect, RectInfo centerRect, String text,
+                  Paint textPaint, Paint bgCenterPaint, Paint bgOtherPaint) {
+        this(x, y);
+        this.leftBall = leftBall;
+        this.rightRect = rightRect;
+        this.centerRect = centerRect;
+        this.text = text;
+        mTextPaint = textPaint;
+        mBgCenterPaint = bgCenterPaint;
+        mBgOtherPaint = bgOtherPaint;
+
+    }
+
+    public BallInfo getLeftBall() {
+        return leftBall;
+    }
+
+    public void setLeftBall(BallInfo leftBall) {
+        this.leftBall = leftBall;
+    }
+
+    public RectInfo getRightRect() {
+        return rightRect;
+    }
+
+    public void setRightRect(RectInfo rightRect) {
+        this.rightRect = rightRect;
+    }
+
+    public RectInfo getCenterRect() {
+        return centerRect;
+    }
+
+    public void setCenterRect(RectInfo centerRect) {
+        this.centerRect = centerRect;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Paint getTextPaint() {
+        return mTextPaint;
+    }
+
+    public void setTextPaint(Paint textPaint) {
+        mTextPaint = textPaint;
+    }
+
+    public Paint getBgCenterPaint() {
+        return mBgCenterPaint;
+    }
+
+    public void setBgCenterPaint(Paint bgCenterPaint) {
+        mBgCenterPaint = bgCenterPaint;
+    }
+
+    public Paint getBgOtherPaint() {
+        return mBgOtherPaint;
+    }
+
+    public void setBgOtherPaint(Paint bgOtherPaint) {
+        mBgOtherPaint = bgOtherPaint;
+    }
+
 
 }
--- app/src/main/java/com/example/dongjunjun/favirite/animator/SubTagView.java
+++ app/src/main/java/com/example/dongjunjun/favirite/animator/SubTagView.java
@@ -3,6 +3,10 @@ package com.example.dongjunjun.favirite.animator;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -13,14 +17,22 @@ import android.view.View;
 
 public class SubTagView extends View {
 
+    Paint mBgCenterPaint;
+    Paint mTextPaint;
+    Paint mBgOtherPaint;
+
+    RectF rectCenter;
+    RectF rectRight;
     SubTag subTag;
 
     public SubTagView(Context context) {
         super(context);
+        setLayerType(View.LAYER_TYPE_SOFTWARE, null);
     }
 
     public SubTagView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        setLayerType(View.LAYER_TYPE_SOFTWARE, null);
     }
 
     public SubTagView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
@@ -55,10 +67,38 @@ public class SubTagView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
+        mTextPaint = subTag.getTextPaint();
+        mBgCenterPaint = subTag.getBgCenterPaint();
+        mBgOtherPaint = subTag.getBgOtherPaint();
+
+        rectCenter = subTag.getCenterRect().toRect();
+        rectRight = subTag.getRightRect().toRect();
+
+        int saved = canvas.saveLayer(null, null, Canvas.ALL_SAVE_FLAG);
+        canvas.drawCircle(subTag.getLeftBall().getX(), subTag.getLeftBall().getY(), subTag.getLeftBall().getR(), mBgOtherPaint);
+        canvas.drawRoundRect(rectRight, subTag.getRightRect().getR(), subTag.getRightRect().getR(), mBgOtherPaint);
+        mBgCenterPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC));
+        canvas.drawRoundRect(rectCenter, subTag.getCenterRect().getR(), subTag.getCenterRect().getR(), mBgCenterPaint);
+        mBgCenterPaint.setXfermode(null);
+        canvas.restoreToCount(saved);
+
+        Paint.FontMetrics metrics = mTextPaint.getFontMetrics();
+        float textCenterVerticalBaselineY = subTag.getCenterRect().getHeight() / 2 - metrics.descent + (metrics.bottom - metrics.top) / 2;
+        canvas.drawText(subTag.getText(), (subTag.getCenterRect().getWidth())/2+ subTag.getCenterRect().getLeft(), textCenterVerticalBaselineY, mTextPaint);
     }
 
     @Override
     protected void onDetachedFromWindow() {
         super.onDetachedFromWindow();
+    }
+
+    public void setSubTag(SubTag subTag) {
+        this.subTag = subTag;
+        invalidate();
+    }
+
+    public SubTag getSubTag() {
+        return subTag;
     }
 }
